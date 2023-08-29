a468fc5fedf1292401af6d66bddd13620e531dbc
vorisxonsobirov <vorisxon.me@gmail.com>
2023-08-29 14:13:30 +0500

    first website

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,27 @@
 // Scripts
 // 
 
- var ism = prompt ("what is your name ");
-alert ("name please");
+//  var ism = prompt ("what is your name ");
+// alert ("name please");
 
- var ism = prompt ("name please");
- if (ism == "") {
-     alert("you must enter your name")
- } 
- var yosh = prompt ("How old are you");
- if (yosh > 18) {
-     alert ("You are over 18 years old!")
- } else {
-    alert ("you are not 18 years old!")
- }
- var email = prompt ("please enter your e-mail");
- if (email == "") {
-    alert("please enter your e-mail")
-} else { 
-       alert ("You didn't write an email")
-     }
- alert ("hello " +  ism + " welcome to my website");
+//  var ism = prompt ("name please");
+//  if (ism == "") {
+//      alert("you must enter your name")
+//  } 
+//  var yosh = prompt ("How old are you");
+//  if (yosh > 18) {
+//      alert ("You are over 18 years old!")
+//  } else {
+//     alert ("you are not 18 years old!")
+//  }
 //  var email = prompt ("please enter your e-mail");
+//  if (email == "") {
+//     alert("please enter your e-mail")
+// } else { 
+//        alert ("You didn't write an email")
+//      }
+//  alert ("hello " +  ism + " welcome to my website");
+// //  var email = prompt ("please enter your e-mail");
 
 window.addEventListener('DOMContentLoaded', event => {
 
